e28c0cce64a41d4969c952f757f7e9f40889ffa3
Pierre-Michel Brown <pm@rotulelongboards.com>
2016-11-25 11:55:54 +0100

    gulp task error handling added
    It will prevent gulp.watch from stopping on error.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
-const gulp  = require('gulp');
+const gulp    = require('gulp');
 const changed = require('gulp-changed');
-const babel = require('gulp-babel');
-const sass  = require('gulp-sass');
+const babel   = require('gulp-babel');
+const sass    = require('gulp-sass');
+
+const logTaskError = function(error) {
+  console.log(error.toString());
+  this.emit('end');
+};
 
 gulp.task('es6', () => {
-    return gulp.src('src/js/**/*.js')
-        .pipe(changed('public/js'))
-        .pipe(babel({
-            presets: ['es2015'],
-            plugins: ['transform-object-assign']
-        }))
-        .pipe(gulp.dest('public/js'));
+  return gulp.src('src/js/**/*.js')
+    .pipe(changed('public/js'))
+    .pipe(babel({
+      presets: ['es2015'],
+      plugins: ['transform-object-assign']
+    }))
+    .on('error', logTaskError)
+    .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('sass', () => {
-    return gulp.src('src/scss/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public/css'));
+  return gulp.src('src/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('default', ['es6', 'sass'], () => {
-    gulp.watch('src/js/**/*.js', ['es6']);
-    gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/js/**/*.js', ['es6']);
+  gulp.watch('src/scss/**/*.scss', ['sass']);
 });
